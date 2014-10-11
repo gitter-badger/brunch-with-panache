f829de87e0b43314a1d367b7ad5da60446b8d715
Josh Habdas <habdas@trunkclub.com>
2014-10-11 11:51:16 -0500

    More strict mode for vanilla JS project assets

--- test/setup.js
+++ test/setup.js
@@ -1,11 +1,15 @@
-require('mocha-as-promised')();
-var chai = require('chai');
-var webdriver = require('selenium-webdriver');
+(function() {
+  'use strict';
 
-global.assert = chai.assert;
-global.expect = chai.expect;
-global.should = chai.should();
+  require('mocha-as-promised')();
+  var chai = require('chai');
+  var webdriver = require('selenium-webdriver');
 
-global.getDriver = function() {
-  return new webdriver.Builder().usingServer('http://localhost:4444').build();
-};
+  global.assert = chai.assert;
+  global.expect = chai.expect;
+  global.should = chai.should();
+
+  global.getDriver = function() {
+    return new webdriver.Builder().usingServer('http://localhost:4444').build();
+  };
+}());
